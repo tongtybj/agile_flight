f62408ea436f1287d99aef94d8be6cd9ed6a4cec
HarukiKozukapenguin <kozukozupenguin@g.ecc.u-tokyo.ac.jp>
2022-04-29 22:34:27 +0900

    change sh and ppo running code for simulation

--- envtest/run_envtest.sh
+++ envtest/run_envtest.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # for move_coeff in 0.5 1
-# do 
+# do
 # for collision_coeff in -0.1 -0.4 -1 -5
 # do
 # for survive_rew in 0.03 0.1 0.3
@@ -10,23 +10,19 @@
 # done
 # done
 # done
-start_time=`date +%s`
-for move_coeff in 0 0.5 
-do 
-for collision_coeff in -0.01 -0.05
-do
-for survive_rew in 0.1 0 -0.1
-do
-for collision_exp_coeff in 1 2
-do
-python3 -m python.run_vision_ppo --render 0 --train 1 \
---move_coeff $move_coeff --collision_coeff $collision_coeff \
---collision_exp_coeff $collision_exp_coeff --survive_rew $survive_rew
-# --check True
+start_time=$(date +%s)
+for move_coeff in 0.5 1; do
+    for collision_coeff in -0.01 -0.05; do
+        for survive_rew in 0.1 0 -0.1; do
+            for collision_exp_coeff in 1 2; do
+                python3 -m python.run_vision_ppo --render 0 --train 1 \
+                    --move_coeff $move_coeff --collision_coeff $collision_coeff \
+                    --collision_exp_coeff $collision_exp_coeff --survive_rew $survive_rew
+                # --check True
+            done
+        done
+    done
 done
-done
-done
-done
-end_time=`date +%s`
+end_time=$(date +%s)
 run_time=$((end_time - start_time))
 echo $run_time
